bbd7e99efc506933788e9f106da858a142909af5
nguyentai219 <nguyentai219@gmail.com>
2026-09-21 12:24:43 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,13 @@
 /* Service Worker — Lãi Suất v1.0.1 */
-const CACHE = 'laisuat-v1.0.2';
+const CACHE = 'laisuat-v1.2.7';
 const ASSETS = [
   './',
   './index.html',
   './manifest.json',
   './icons/icon-192.png',
   './icons/icon-512.png',
+  './icons/apple-touch-icon.png',
+  './icons/favicon-32.png',
   'https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro:wght@300;400;500;600;700;800&display=swap'
 ];
 
